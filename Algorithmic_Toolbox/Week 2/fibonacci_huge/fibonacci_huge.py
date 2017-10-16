9cb4b8c18e31c6d89b0f23857cbfc929d98be958
Pranay Agarwal <agarwalpranay93@gmail.com>
2017-10-16 14:59:10 -0700

    saving week 3 progress

--- Algorithmic_Toolbox/Week 2/fibonacci_huge/fibonacci_huge.py
+++ Algorithmic_Toolbox/Week 2/fibonacci_huge/fibonacci_huge.py
@@ -13,7 +13,35 @@ def get_fibonacci_huge_naive(n, m):
 
     return current % m
 
+def get_fibonacci(n):
+    if n <= 1:
+        return n
+
+    previous = 0
+    current = 1
+
+    for _ in range(n - 1):
+        previous , current = current, previous + current
+
+    return current
+
+
+def periodicity_length(m):
+    previous = 0
+    current = 1
+    for i in range(m * m + 1):
+        previous , current = current, (previous + current) % m
+        if previous == 0 and current == 1:
+            return i + 1
+
+def get_fibonacci_huge_fast(n, m):
+    remainder = n % periodicity_length(m)
+    return get_fibonacci(remainder) % m
+
+
 if __name__ == '__main__':
-    input = sys.stdin.read();
-    n, m = map(int, input.split())
+#    input = sys.stdin.read();
+#    n, m = map(int, input.split())
+    n = int(input())
+    m = int(input())
     print(get_fibonacci_huge_naive(n, m))
--- Algorithmic_Toolbox/Week 2/lcm/lcm.py
+++ Algorithmic_Toolbox/Week 2/lcm/lcm.py
@@ -2,14 +2,37 @@
 import sys
 
 def lcm_naive(a, b):
-    for l in range(1, a*b + 1):
-        if l % a == 0 and l % b == 0:
-            return l
+	for l in range(1, a*b + 1):
+		if l % a == 0 and l % b == 0:
+			return l
+	return a*b
 
-    return a*b
+def gcd_smart(a, b):
+	if a >= b:
+		dividend = a
+		divisor = b
+	else:
+		dividend = b
+		divisor = a
+
+	while divisor != 0:
+		remainder = dividend % divisor
+		dividend = divisor
+		divisor = remainder
+
+	return dividend
+
+def lcm_smart(a, b):
+	lcm = a * b // gcd_smart(a, b)
+	return lcm
+
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    a, b = map(int, input.split())
-    print(lcm_naive(a, b))
+#	a = int(input())
+#	b = int(input())
+	input = sys.stdin.read()
+	a, b = map(int, input.split())
+	print(lcm_smart(a, b))
+
+
 
